77209a247a41cbbe676ea53e3fc7c216a990f99e
Juan Quinche <juan.quinche@hotmail.com>
2022-10-13 22:07:44 -0500

    role retrieving fixed

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,13 +1,16 @@
 const user = require('../models/user')
+const {StatusCodes} = require('http-status-codes')
 const {NotFoundError, UnauthorizedError, BadRequestError} = require('../errors')
 
 const getAllUsers = async (req, res) => {
 
-    if(user.role != 'admin')
+
+    if(req.user.role != 'admin')
     {
         throw new UnauthorizedError('User not authorize to access resources')
     }
 
+    res.status(StatusCodes.CREATED).send('Users displayed for admin')
 }
 
 module.exports = {
