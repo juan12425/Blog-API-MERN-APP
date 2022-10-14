822f89bc16c897d99291c1e8abdddc131b64b83a
Juan Quinche <juan.quinche@hotmail.com>
2022-10-13 21:32:45 -0500

    Auth corrections

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,17 +1,42 @@
 const User = require('../models/user')
 const { StatusCodes } = require('http-status-codes')
-
+const {NotFoundError, UnauthorizedError, BadRequestError} = require('../errors')
 
 const register = async (req, res) => {
 
+    if(req.body.role)
+    {
+        throw new UnauthorizedError('Not authorized to assaign roles')
+    }
+
     const user = await User.create({...req.body})
     const token = user.createJWT()
-    res.status(StatusCodes.CREATED).json({user:{name: user.name}, token})
+    res.status(StatusCodes.CREATED).json({user:{name: user.name}, token, role: user.role})
     
 }
 
-const login = (req, res) => {
-    res.status(200).send('Login User')
+const login = async (req, res) => {
+    
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+
+    if(!password || !email)
+    {
+        throw new BadRequestError('Please provide a password and an email')
+    }
+
+    if(!user)
+    {
+        throw new NotFoundError('User could not be found')
+    }
+    
+    if(!user.compare(password))
+    {
+        throw new UnauthorizedError('Not authorized')
+    }
+
+
+    res.send('User logged successfully')
 }
 
 module.exports = {
