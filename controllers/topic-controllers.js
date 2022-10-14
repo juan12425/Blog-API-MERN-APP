88d6ce7e25521d72a310390ad46079a8024d0da3
Juan Quinche <juan.quinche@hotmail.com>
2022-10-14 12:05:33 -0500

    Error handler added

--- controllers/topic-controllers.js
+++ controllers/topic-controllers.js
@@ -1,9 +1,16 @@
 const Topic = require('../models/topic')
 const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors')
 
 const createTopic = async (req, res) => {
     const userId = req.user.userId
     const topic = await Topic.create({...req.body, createdBy: userId})
+
+    if(!topic)
+    {
+        throw new BadRequestError('Topic was not created, please try again later')
+    }
+
     res.status(StatusCodes.CREATED).json({topic})
 }
 
