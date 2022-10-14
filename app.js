9b88c0a9ab56cde7360f05a0a9da6bf06f3612ea
Juan Quinche <juan.quinche@hotmail.com>
2022-10-14 15:44:52 -0500

    More packages added

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 require('express-async-errors');
 
+const helmet = require('helmet');
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 const connectDB = require('./db/connect')
@@ -14,6 +19,14 @@ const authMiddleware = require('./middleware/authentication')
 const isAdminMiddleware = require('./middleware/is-admin')
 
 app.use(express.json())
+
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 app.get('/', (req, res) => {
     res.send('Blog API');
