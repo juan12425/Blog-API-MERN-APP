e46c55309a6b0bbab8e379f0ee10f8e3b2f40105
Juan Quinche <juan.quinche@hotmail.com>
2022-10-15 17:31:18 -0500

    Home page completed

---
+++ client/src/features/footer/footer.js
@@ -0,0 +1,7 @@
+import './footer.css'
+
+export function Footer(){
+    return(<div id="footer">
+        <h3>©2022 The Tech Blog</h3>
+    </div>)
+}
--- client/src/features/home/home.js
+++ client/src/features/home/home.js
@@ -1,8 +1,24 @@
+import './home.css'
+import {Link} from 'react-router-dom'
+import {Footer} from '../footer/footer'
 
 export function Home(){
     return(
         <div>
-            <p>Welcome to the home website</p>
+            <div id="spotlight">
+                <div id="blog-cover-div">
+                    <img id="blog-cover" src={require('../../images/blog-cover.png')}/>
+                </div>
+                <div id="blog-description">
+                    <p id="description-paragraph">This is The Tech Blog, a MERN web application.</p>
+                    <p id="description-foot-note">- Compromised with the best user experience</p>
+                </div>
+            </div>
+            <div id="login-reg-links">
+                <Link className="link-home" to="/register">Register</Link>
+                <Link className="link-home" to="/login">Login</Link>
+            </div>
+            <Footer />
         </div>
     )
 }
