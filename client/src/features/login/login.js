62781279394a468a812d340de1c7fe32e7cb92a1
Juan Quinche <juan.quinche@hotmail.com>
2022-10-15 19:36:56 -0500

    Login corrected

--- client/src/features/login/login.js
+++ client/src/features/login/login.js
@@ -1,5 +1,37 @@
-export function Login(){
+import './login.css'
+import {useState} from 'react'
+
+export function Login(props){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleChange = (event) => {
+        const {value, name} = event.target
+        
+        if(name === 'email')
+        {
+            setEmail(value)
+        }
+
+        if(name === 'password')
+        {
+            setPassword(value)
+        }
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        
+    }
+
     return(<div>
-        Welcome to the login page
+        <div id="form-div" onSubmit={handleSubmit}>
+            <form>
+                <h1>The Tech Blog Login</h1>
+                <input type="email" placeholder="Email" value={email} name="email" onChange={handleChange} required/>
+                <input type="password" placeholder="Password"  value={password} name="password" onChange={handleChange} required/>
+                <button type="submit">Login</button>
+            </form>      
+        </div>
     </div>)
 }
