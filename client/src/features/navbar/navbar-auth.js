e05ff4c65ec55abe679c52fdba3d2c8c4e8c9dbe
Juan Quinche <juan.quinche@hotmail.com>
2022-10-19 12:22:34 -0500

    Navbar fixed

--- client/src/features/navbar/navbar-auth.js
+++ client/src/features/navbar/navbar-auth.js
@@ -5,7 +5,7 @@ export function Navbar(){
     return(
         <div id="navbar">
             <ul>
-                <li><Link to="/resources">Dashboard</Link></li>
+                <li><Link to="/resources/dashboard">Dashboard</Link></li>
                 <li><Link to="/resources/profile">Profile</Link></li>
                 <li><Link to="/resources/log-out">Log Out</Link></li>
             </ul>
