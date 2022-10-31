8e6a24d5c1e07feebecad48eaa5b0b38d1b955df
Juan Quinche <juan.quinche@hotmail.com>
2022-10-30 22:01:49 -0500

    Post edition improved

--- client/src/features/posts/post/post.js
+++ client/src/features/posts/post/post.js
@@ -5,21 +5,23 @@ import { useSelector } from 'react-redux'
 import {sendDelete, sendUpdate} from '../../../crud/crud'
 
 export function Post(props){
-    const {id, createdBy, userId, createdAt, username, text} = props 
+    const {id, createdBy, userId, createdAt, username} = props 
     const {token, role} = useSelector(selectUserInfo)
     const [deleted, setDeleted] = useState(false)
     const [modifying, setModifying] = useState(false)
     const [newName, setNewName] = useState(props.name)
     const [name, setName] = useState(props.name)
     const date = (new Date(createdAt)).toDateString()
+    const [newText, setNewText] = useState(props.text)
+    const [text, setText] = useState(props.text)
 
     const deletePost = async (id) => {
         const response = await sendDelete('posts', id, '', token)
         return response
     }
 
-    const updatePostName = async (id, name) => {
-        const response = await sendUpdate('posts', id, '', {name}, token)
+    const updateName = async (id, name, text) => {
+        const response = await sendUpdate('posts', id, '', {name, text}, token)
         return response
     }
 
@@ -38,10 +40,11 @@ export function Post(props){
 
     const handleSubmitModify = (event) => {
         event.preventDefault()
-        updatePostName(id, newName).then(response => {
+        updateName(id, newName, newText).then(response => {
             if(response.msg === 'Update was successful')
             {
                 setName(newName)
+                setText(newText)
             }
         })
         setModifying(false)
@@ -66,17 +69,23 @@ export function Post(props){
             {modifying ? 
                 <form onSubmit={handleSubmitModify}> 
                     <input className="modify-input" type="text" value={newName} onChange={({target})=> setNewName(target.value)} placeholder="New name" required/>
+                    <button className="modify confirm-edit" type="submit">Confirm changes</button>
                 </form> : 
             <h2 className="post-title" onClick={()=>handleClickPostTitle(id)}>{name}</h2>}
             <p>{username}</p>
             <p>{date}</p>
             <div id={id} className="post-info">
                 {(userId === createdBy || role !== 'client') && (<div>
-                    <button className="topic-button modify" onClick={handleClickModify}>modify</button>
-                    <button className="topic-button delete" onClick={handleClickDelete}>delete</button>
+                    {modifying === false && <><button className="topic-button modify" onClick={handleClickModify}>modify</button>
+                    <button className="topic-button delete" onClick={handleClickDelete}>delete</button></>}
                 </div>)}
                 <hr className="hr-post"/>
-                <p className="post-inner-text">{text}</p>
+                {modifying ? 
+                    <form onSubmit={handleSubmitModify}>
+                        <textarea className="textarea-modify-post" value={newText} onChange={({target}) => setNewText(target.value)}/>
+                    </form> :
+                    <p className="post-inner-text">{text}</p>
+                }
             </div>
         </div>
     )
