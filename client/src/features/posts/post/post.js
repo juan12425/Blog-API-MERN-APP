24b755fe52587cd5afc6474a1f1c0b826a2c3c4d
Juan Quinche <juan.quinche@hotmail.com>
2022-11-01 12:51:35 -0500

    Post edit restructurized

--- client/src/features/posts/post/post.js
+++ client/src/features/posts/post/post.js
@@ -127,6 +127,7 @@ export function Post(props){
             <p>{date}</p>
 
             <div id={id} className="post-info">
+                
                 {(userId === createdBy || role !== 'client') && (<div>
                     {modifying === false && <><button className="topic-button modify" onClick={handleClickModify}>modify</button>
                     <button className="topic-button delete" onClick={handleClickDelete}>delete</button></>}
@@ -136,34 +137,37 @@ export function Post(props){
                     <form onSubmit={handleSubmitModify}>
                         <textarea className="textarea-modify-post" value={newText} onChange={({target}) => setNewText(target.value)}/>
                     </form> :
-                    <>
-                        <div className="post-inner-text">
-                            <p className="post-text-first">{text}</p>
-
-                            {replies.map((reply, index) => {
-                                const {text, username, createdAt} = reply
-                                const dateReply = (new Date(createdAt)).toDateString()
-                                return (<div key={index}>
-                                    <hr className="hr-post"/>
-                                    <p className="reply-text">{text}</p>
-                                    <div className="reply-extra-info">
-                                        <span>{username}</span><br/>
-                                        <span>{dateReply}</span>
-                                    </div>
-                                </div>)
-                            })}
-
-                            <div>
-                                <button className="add-reply-button" onClick={() => handleDisplayFormReply(`${id}-form`)}>+ Add Reply</button>
-                                <form id={`${id}-form`} className="reply-form" onSubmit={handleSubmitReply}>
-                                    <textarea className="textarea-reply" value={replyText} onChange={handleChangeReplyText}/>
-                                    <button className="modify confirm-edit" type="submit">Add reply</button>
-                                </form>
-                            </div>
-
-                        </div>
-                    </>
+                    
+                    <div className="post-inner-text">
+                        <p className="post-text-first">{text}</p>
+                    </div>
                 }
+
+                <div>
+                    {replies.map((reply, index) => {
+                        const {text, username, createdAt} = reply
+                        const dateReply = (new Date(createdAt)).toDateString()
+                        return (<div key={index}>
+                            <hr className="hr-post"/>
+                            <p className="reply-text">{text}</p>
+                            <form>
+                                <textarea></textarea>
+                            </form>
+                            <div className="reply-extra-info">
+                                <span>{username}</span><br/>
+                                <span>{dateReply}</span>
+                            </div>
+                        </div>)
+                    })}
+
+                    <div>
+                        <button className="add-reply-button" onClick={() => handleDisplayFormReply(`${id}-form`)}>+ Add Reply</button>
+                        <form id={`${id}-form`} className="reply-form" onSubmit={handleSubmitReply}>
+                            <textarea className="textarea-reply" value={replyText} onChange={handleChangeReplyText}/>
+                            <button className="modify confirm-edit" type="submit">Add reply</button>
+                        </form>
+                    </div>
+                </div>        
             </div>
         </div>
     )
