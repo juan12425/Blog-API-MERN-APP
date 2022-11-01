4dcc2e8b24674c1536e22e5bfded976532948bd7
Juan Quinche <juan.quinche@hotmail.com>
2022-11-01 18:43:45 -0500

    Profile

--- client/src/features/profile/profile.js
+++ client/src/features/profile/profile.js
@@ -1,5 +1,15 @@
+import './profile.css'
+import { useSelector } from 'react-redux'
+import { selectUserInfo } from '../user/user-slice'
+
 export function Profile(){
-    return(<div>
-        hello
+    const {email, username, role} = useSelector(selectUserInfo)
+
+    return(<div id="profile">
+        <ul id="profile-list">
+            <li>Email: {email}</li>
+            <li>Username: {username}</li>
+            <li>Range: {role}</li>
+        </ul>
     </div>)
 }
