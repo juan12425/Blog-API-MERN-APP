7f5ab57c068af182454285763c87fd6d9191f27f
Juan Quinche <juan.quinche@hotmail.com>
2022-11-01 18:00:29 -0500

    Posts separated

---
+++ client/src/features/posts/posts-display.js
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom'
+import { Post } from './post/post'
+
+export function PostsDisplay({posts, data, functions}){
+    const {topicName, newPostName, newPostText, userId, relatedTopicId} = data
+    const {handleClickNewPost, handleSubmitNewPost, setNewPostName, setNewPostText} = functions
+
+    return(<>
+        <h1><NavLink to='../' className="topics-link">Topics</NavLink> / {topicName}</h1>
+        <div id="create-new-topic">
+            <button id='create-new-topic-button' onClick={handleClickNewPost}>+ New Post</button>
+            <form id="form-new-topic" onSubmit={handleSubmitNewPost} style={{display: 'none'}}>
+                <input type="text" placeholder="Enter a post name" value={newPostName} onChange={({target})=>setNewPostName(target.value)} required/>
+                <textarea style={{width: '90%', height: '200px'}} placeholder="Write your ideas" value={newPostText} onChange={({target})=>setNewPostText(target.value)} required/>
+                <button type="submit">Create a New Post</button>
+            </form>
+        </div>  
+
+        {posts.map((post, index) => {
+            const {_id, name, createdBy, createdAt, username, text, isReply} = post
+            if(isReply)
+            {
+                return
+            }
+            return <Post id={_id} name={name} createdBy={createdBy} userId={userId} createdAt={createdAt} username={username} text={text} relatedTopic={relatedTopicId} key={index}/> 
+        }).reverse()}
+    </>)
+}
--- client/src/features/posts/posts.js
+++ client/src/features/posts/posts.js
@@ -2,10 +2,9 @@ import {useEffect} from 'react'
 import { useSelector } from 'react-redux'
 import {selectUserInfo} from '../user/user-slice'
 import { useState } from 'react'
-import { Post } from './post/post'
 import {sendGet, sendPost} from '../../crud/crud'
 import { useSearchParams } from 'react-router-dom'
-import { NavLink } from 'react-router-dom'
+import { PostsDisplay } from './posts-display'
 
 export function Posts(){
     
@@ -62,24 +61,18 @@ export function Posts(){
         })
     }, [])
 
-    return(<>
-        <h1><NavLink to='../' className="topics-link">Topics</NavLink> / {topicName}</h1>
-        <div id="create-new-topic">
-            <button id='create-new-topic-button' onClick={handleClickNewPost}>+ New Post</button>
-            <form id="form-new-topic" onSubmit={handleSubmitNewPost} style={{display: 'none'}}>
-                <input type="text" placeholder="Enter a post name" value={newPostName} onChange={({target})=>setNewPostName(target.value)} required/>
-                <textarea style={{width: '90%', height: '200px'}} placeholder="Write your ideas" value={newPostText} onChange={({target})=>setNewPostText(target.value)} required/>
-                <button type="submit">Create a New Post</button>
-            </form>
-        </div>  
-
-        {posts.map((post, index) => {
-            const {_id, name, createdBy, createdAt, username, text, isReply} = post
-            if(isReply)
-            {
-                return
-            }
-            return <Post id={_id} name={name} createdBy={createdBy} userId={userId} createdAt={createdAt} username={username} text={text} relatedTopic={relatedTopicId} key={index}/> 
-        }).reverse()}
-    </>)
+    return <PostsDisplay 
+        posts={posts}
+        data={{topicName, 
+            newPostName, 
+            newPostText,
+            userId,
+            relatedTopicId
+        }}
+        functions={{handleClickNewPost,
+            handleSubmitNewPost,
+            setNewPostName,
+            setNewPostText
+        }}
+    />
 }
