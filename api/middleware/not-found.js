4961f9d8999a31d5a81efae9da5b0a1a31f0da6e
Juan Quinche <juan.quinche@hotmail.com>
2022-11-03 11:29:55 -0500

    Not found sends file

--- api/middleware/not-found.js
+++ api/middleware/not-found.js
@@ -1,3 +1,5 @@
-const notFound = (req, res) => res.status(404).send('Route does not exist')
+const path = require('path');
+
+const notFound = (req, res) => res.sendfile(path.resolve(__dirname, '../../client/build'))
 
 module.exports = notFound
