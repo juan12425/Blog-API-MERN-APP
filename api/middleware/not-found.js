0993af991ba60a1e73f5396bbeb589c387c5d3e3
Juan Quinche <juan.quinche@hotmail.com>
2022-11-03 11:40:10 -0500

    index served

--- api/middleware/not-found.js
+++ api/middleware/not-found.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
-const notFound = (req, res) => res.sendfile(path.resolve(__dirname, '../../client/build'))
+const notFound = (req, res) => res.sendfile(path.resolve(__dirname, '../../client/build/index.html'))
 
 module.exports = notFound
