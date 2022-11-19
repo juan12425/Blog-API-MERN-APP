7a889896204d38cd8948ed1c8fa32b382c3065b8
Juan Quinche <juan.quinche@hotmail.com>
2022-11-19 13:54:49 -0500

    Connection function replaced

--- api/db/connect.js
+++ api/db/connect.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose')
 
-const connectDB = (URL) => {
-    console.log(URL)
-    mongoose.connect(URL,{
-        useNewUrlParser: false,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-    })
+const connectDB = (URI) => {
+    console.log(URI)
+    mongoose.createConnection('mongodb://localhost:27017/test');
 }
 
 module.exports = connectDB
