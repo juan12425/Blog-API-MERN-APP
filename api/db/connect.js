1e1f301676cf54992c0f88410b97fa7130e56630
Juan Quinche <juan.quinche@hotmail.com>
2022-11-19 14:00:22 -0500

    Connection changed

--- api/db/connect.js
+++ api/db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = (URL) => {
     mongoose.connect(URL,{
-        useNewUrlParser: false,
+        useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
         useUnifiedTopology: true,
