a6ba42fd3c14b3c9ef900a596554c3c9fec89ccd
Juan Quinche <juan.quinche@hotmail.com>
2022-11-19 12:39:53 -0500

    DB options readded

--- api/db/connect.js
+++ api/db/connect.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
 const connectDB = (URL) => {
-    mongoose.connect(URL)
+    mongoose.connect(URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
 }
 
 module.exports = connectDB
